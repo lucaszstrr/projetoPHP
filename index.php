f98f5b0d2ae2c534aea731e15a0310daea587333
lucaszstrr <lucaaszstr@gmail.com>
2025-02-11 17:20:09 -0300

    Função novo cadastro

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 
+    //Função de login
     function login(){
 
         echo "Usuário: " .PHP_EOL;
@@ -8,13 +9,25 @@
         $senha = readline();
 
         if($usuario === 'lucas' && $senha === '123'){
-            logAlteracao("O usuário $usuario entrou.");
+            logAlteracoes("O usuário $usuario entrou.");
             return $usuario;
         }
 
         echo "O usuário ou a senha estão incorretos" .PHP_EOL;
         return false;
 
+    }
+
+    function cadastro(){
+
+        $novoUsuario = readline("Digite o novo usuário: ");
+        $novaSenha = readline("Digite a nova senha: ");
+        //Essa função abaixo é usada pra colocar strings em um arquivo
+        //(nome do arquivo; o que será escrito no arquivo)
+        //o FILE_APPEND é para não sobrescrever o arquivo, ele vai escrever no arquivo já existente
+        file_put_contents('usuarios.txt', '$novoUsuario:$novaSenha <br>', FILE_APPEND);
+        logAlteracoes("O usuário $novoUsuario foi cadastrado.");
+        echo "Usuário cadastrado";
     }
 
 
